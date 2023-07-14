c0a15f64334d443b8adc8e46f10b6e6aa2b585e1
tong <tong@test.com>
2023-07-14 17:56:54 +0800

    2023-07-14

---
+++ C Primer Plus 6/02/2.11 practice.c
@@ -0,0 +1,11 @@
+//
+// Created by Administrator on 2023/7/14.
+//
+#include <stdio.h>
+int main(void) /* 该程序打印一年有多少周 */
+{
+    int s;
+    s = 56;
+    printf("There are %d weeks in a year.\n", s);
+    return 0;
+}
